d7faccdedd7714a845f2a43d4c61c270b1dabff5
Luna Sarmiento <selene942008@hotmail.com>
2021-10-21 20:10:30 +0200

    login component

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,16 @@
+import { RouteComponentProps } from "react-router"
+import "./styles.css"
+
+const Login = ({history}: RouteComponentProps) => {
+    return(
+        <div className="big-container">
+            <div className="content-container">
+                <div className="cover-content">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
