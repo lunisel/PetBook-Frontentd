d722edc75e05e15c383606792073f9361bd05991
Luna Sarmiento <selene942008@hotmail.com>
2021-10-25 17:32:33 +0200

    added home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route, RouteComponentProps } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  RouteComponentProps,
+  Redirect,
+} from "react-router-dom";
 import Login from "./components/login/Login";
-import Signup from "./components/registration/Signup"
+import Signup from "./components/registration/Signup";
+import Home from "./components/home/Home";
 import "./App.css";
+import { useSelector } from "react-redux";
+import { reduxStateInt, userInt } from "./utils/interfaces";
 
 function App() {
+  const user: userInt | null = useSelector(
+    (state: reduxStateInt) => state.user.currentUser
+  );
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -22,6 +34,19 @@ function App() {
             <Signup {...routerProps} />
           )}
         />
+        <Route exact path="/">
+          {user ? (
+            <Route
+              exact
+              path="/"
+              render={(routerProps: RouteComponentProps) => (
+                <Home {...routerProps} />
+              )}
+            />
+          ) : (
+            <Redirect to="/login" />
+          )}
+        </Route>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,7 @@
+import { RouteComponentProps } from "react-router";
+
+const Home = ({ history }: RouteComponentProps) => {
+  return <div className="home-container"></div>;
+};
+
+export default Home;
