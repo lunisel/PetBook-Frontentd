0e78e1d919f3da591302362a096848baaa1b830b
Luna Sarmiento <selene942008@hotmail.com>
2021-10-26 13:35:03 +0200

    navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,137 @@
-import { RouteComponentProps, withRouter } from "react-router-dom"
-import {BiLogOut} from "react-icons/bi"
-import { useDispatch } from "react-redux"
-import {disconnectUser} from "../redux/actions/user"
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
+import { BiLogOut } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { disconnectUser } from "../redux/actions/user";
+import {
+  FaPaw,
+  FaHome,
+  FaUserCircle,
+  FaBook,
+  FaNewspaper,
+} from "react-icons/fa";
+import { IoIosPaperPlane } from "react-icons/io";
+import { useEffect, useState } from "react";
 
-const Navbar = ({history}: RouteComponentProps) => {
-    const dispatch = useDispatch()
+const Navbar = ({ history, location }: RouteComponentProps) => {
+  const dispatch = useDispatch();
+  const [vw, setVW] = useState<Number>(window.innerWidth);
 
-    return(
-        <div className="nav-container">
-            <BiLogOut onClick={()=> {
-                dispatch(disconnectUser())
-                setTimeout(()=>{history.push("/login")}, 2000)
-            }}/>
+  const updateWindowDimensions = () => {
+    const newWidth = window.innerWidth;
+    setVW(newWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowDimensions);
+  }, []);
+
+  return (
+    <div className="nav-container">
+      <FaPaw className={vw <= 1000 ? " d-none" : "nav-icon"} />
+      <h3 className={vw <= 1000 ? " d-none" : "text-logo-nav"}>PetBook</h3>
+      <Link
+        to="/"
+        className={
+          location.pathname === "/" ? "link-container active" : "link-container"
+        }
+      >
+        <FaHome className="link-icon-nav" />
+        <div
+          className={
+            (location.pathname === "/" ? "link-nav active" : "link-nav") +
+            (vw <= 1000 ? " d-none" : "")
+          }
+        >
+          Home
         </div>
-    )
-}
+      </Link>
 
-export default withRouter(Navbar)
+      <Link
+        to="/me"
+        className={
+          location.pathname === "/me"
+            ? "link-container active"
+            : "link-container"
+        }
+      >
+        <FaUserCircle className="link-icon-nav" />
+
+        <div
+          className={
+            (location.pathname === "/me" ? "link-nav active" : "link-nav") +
+            (vw <= 1000 ? " d-none" : "")
+          }
+        >
+          My Profile
+        </div>
+      </Link>
+      <Link
+        to="/notes"
+        className={
+          location.pathname === "/notes"
+            ? "link-container active"
+            : "link-container"
+        }
+      >
+        <FaBook className="link-icon-nav" />
+        <div
+          className={
+            (location.pathname === "/notes" ? "link-nav active" : "link-nav") +
+            (vw <= 1000 ? " d-none" : "")
+          }
+        >
+          Notes
+        </div>
+      </Link>
+      <Link
+        to="/feed"
+        className={
+          location.pathname === "/feed"
+            ? "link-container active"
+            : "link-container"
+        }
+      >
+        <FaNewspaper className="link-icon-nav" />
+        <div
+          className={
+            (location.pathname === "/feed" ? "link-nav active" : "link-nav") +
+            (vw <= 1000 ? " d-none" : "")
+          }
+        >
+          Feed
+        </div>
+      </Link>
+      <Link
+        to="/messages"
+        className={
+          location.pathname === "/messages"
+            ? "link-container active"
+            : "link-container"
+        }
+      >
+        <IoIosPaperPlane className="link-icon-nav" />
+        <div
+          className={
+            (location.pathname === "/messages"
+              ? "link-nav active"
+              : "link-nav") + (vw <= 1000 ? " d-none" : "")
+          }
+        >
+          Messages
+        </div>
+      </Link>
+
+      <BiLogOut
+        className="log-out-icon"
+        onClick={() => {
+          dispatch(disconnectUser());
+          setTimeout(() => {
+            history.push("/login");
+          }, 2000);
+        }}
+      />
+    </div>
+  );
+};
+
+export default withRouter(Navbar);
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,6 @@
 import { RouteComponentProps } from "react-router";
 import Navbar from "../Navbar"
+import "./home.css"
 
 const Home = ({ history }: RouteComponentProps) => {
   return <div className="home-container">
